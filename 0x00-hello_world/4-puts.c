f74b59fb73b150d31194fcd8ab933848ef31a5bd
Muna-Redi <francischym@gmail.com>
2022-06-02 16:44:18 -0700

    betty documentation style used

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
-/**
-*file - 4-puts.c
-*Author: Francis Francis
-*Description: prints an a line of text, "Programming is like building a multilingual puzzle"
-*to the standard output.
-*/
 #include <stdio.h>
+
 /**
-*main: entry point
-*
-*Description: prints the text given to the puts function
-*
-*retun: Aways 0 (success)
-*/
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
