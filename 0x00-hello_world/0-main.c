890b53f4766a8a0928b70f073525f2cdc8193ddd
Muna-Redi <francischym@gmail.com>
2022-06-02 19:39:56 -0700

    created 0-main.c file

---
+++ 0x00-hello_world/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point of the C code
+*
+* Description: prints a string to the standard output
+*
+* Return: This returns an int 0 to indicate success
+*/
+int main(void)
+{
+/**
+* John Cena's catch phrase would be printed
+*/
+	printf("You can't C me\n");
+	return (0);
+}
