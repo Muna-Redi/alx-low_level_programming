172688a96303cfd7d3788c1b4b9f9d7650b97822
Muna-Redi <francischym@gmail.com>
2022-06-08 00:03:49 -0700

    added some branching to achieve the desired results

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,24 @@
 */
 void print_to_98(int n)
 {
+	if (n == 98)
+	{
+		printf("%d ", n);
+	}
 	if (n > 98)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
+			if (n != 97)
+			{
+			printf("%d", n);
+			}
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			else if (n == 98)
+				printf(" ");
 			n--;
 		}
 	}
@@ -23,7 +36,13 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			else if (n == 98)
+				printf(" ");
 			n++;
 		}
 	}
