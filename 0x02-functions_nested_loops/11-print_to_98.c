f8dc3ea42c7a9b7258bc9aefda4a52e1812fe8ca
Muna-Redi <francischym@gmail.com>
2022-06-08 07:13:15 -0700

    removed some branching

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,30 +11,21 @@
 */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n < 98)
 	{
-		do {
-			if (n  == 98)
-			{
-				printf("%d ", n);
-				break;
-			}
-			printf("%d, ", n);
-			n--;
-		} while (n != 97);
-	}
-	else if (n < 98)
-	{
-		do {
-			if (n == 98)
-			{	printf("%d ", n);
-				break;
-			}
+		while (n < 98)
+		{
 			printf("%d, ", n);
 			n++;
-		} while (n != 99);
+		}
 	}
-	else
-		printf("98\n");
-	return ;
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
