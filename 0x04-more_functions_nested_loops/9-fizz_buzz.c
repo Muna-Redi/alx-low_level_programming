8e56005c6c18126fd85869ab12ea13bf58d77eb6
Muna-Redi <francischym@gmail.com>
2022-06-09 20:31:09 -0700

    printin 1 to 100 and fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main -  prints numbers 1 t0 100 and fizzbuzz for multiples
+* of 3 and 5
+*
+* Description: prints 1 t0 100 with fizz and buzz for multiples of 3 and 5
+*
+* Return: 0 is success
+*/
+int main(void)
+{
+	int n = 0;
+	while (n <= 100)
+	{
+		if (n % 5 == 0 && n % 3 == 0)
+			printf("FizzBuzz ");
+		else if (n % 5 == 0)
+		{
+			if (n == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
+
