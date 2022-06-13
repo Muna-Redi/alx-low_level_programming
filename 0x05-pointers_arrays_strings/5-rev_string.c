70de0c54059fcc36782fc7bfdcefa02f13359c2d
Muna-Redi <francischym@gmail.com>
2022-06-13 10:17:11 -0700

    fixing rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,23 @@
  */
 void rev_string(char *s)
 {
-	int n, p, r;
+	int n, i, p, r;
 
 	n = 0;
 	while (s[n] != '\0')
 	{
 		n++;
 	}
+	i = n / 2;
 	r = n - 1;
 	p = r;
 	while (p >= 0)
 	{
-		s[r -p] = s[p];
+		s[r - p] = s[p];
+		if (p == i)
+		{
+			s[p] = s[r - p];
+		}
 		p--;
 	}
 }
