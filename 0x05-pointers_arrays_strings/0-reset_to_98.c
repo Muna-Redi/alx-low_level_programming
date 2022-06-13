69d38fd11868adfa5e1984188aa23deef79fbd36
Muna-Redi <francischym@gmail.com>
2022-06-13 05:35:39 -0700

    closed a statement and spelt reset right

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * rest_to_98 - this resets the valuenof an int to 98
+ * reset_to_98 - this resets the valuenof an int to 98
  *
- * @*n: a pointer to an int variable type
+ * @n: a pointer to an int variable type
  *
  * Return: return is void.
  */
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
 
