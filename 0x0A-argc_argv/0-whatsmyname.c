f8236b6bb23e416400e54eb966145531ea7ec98d
Muna-Redi <francischym@gmail.com>
2022-06-24 06:47:58 -0700

    prints name of programe

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: pointer of arrays
+ *
+ * Return: 0 success
+ */
+int main(char argc, char *argv[])
+{
+		printf("%s\n", argv[0]);
+	return (0);
+}
