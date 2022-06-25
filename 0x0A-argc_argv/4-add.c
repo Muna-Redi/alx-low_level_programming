8a943877802db04eab14c8d38c916ed9f06c8ca7
Muna-Redi <francischym@gmail.com>
2022-06-24 18:36:12 -0700

    adding positive arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <string.h>
 
 /**
  * main - Entry point
@@ -13,25 +12,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int add = 0, x = 0, i = 0;
-	int num[argc];
-	char s[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-;,_";
-
+	int add = 0, x = 0;
 	for (x = 0; x < argc; x++)
 	{
-		num[x] = atoi(argv[x]);
 		if (atoi(argv[x]) != 0 && argv[x] != 0)
 		{
-			add += num[x];
+			add += atoi(argv[x]);
 			continue;
-		}
-		for (i = 0; i < 56; i++)
-		{
-			if (strchr(argv[x], s[i]) && atoi(argv[x]))
-			{
-				printf("Error\n");
-				return (1);
-			}
 		}
 	}
 	if (argc < 2)
