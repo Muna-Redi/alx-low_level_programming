94782cfa444240d638199237a61e006c7d82f9c9
Muna-Redi <francischym@gmail.com>
2022-07-07 19:16:54 +0100

    returning 0 if n is less than 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,9 +10,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum;
 	
 	sum = 0;
-	va_start(num, n);
 	if (n == 0)
 		return (0);
+	va_start(num, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(num, int);
