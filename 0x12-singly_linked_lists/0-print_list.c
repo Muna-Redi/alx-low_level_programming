942ac8dd8213b46e357b0e0d7dab8c779b9fb24f
Muna-Redi <francischym@gmail.com>
2022-07-14 15:16:02 -0700

    betty indentation

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,18 @@
 */
 size_t print_list(const list_t *h)
 {
-        const list_t *ph;
-        size_t n = 0;
-        
-        ph = h;
-        while (ph != NULL)
-        {
-                if (ph->str == NULL)
-                        printf("[%d] (nil)\n", ph->len);
-                else
-                        printf("[%d] %s\n", ph->len, ph->str);
-                ph = ph->next;
-                n++;
-        }
-        return (n);
+	const list_t *ph;
+	size_t n = 0;
+
+	ph = h;
+	while (ph != NULL)
+	{
+		if (ph->str == NULL)
+			printf("[%d] (nil)\n", ph->len);
+		else
+			printf("[%d] %s\n", ph->len, ph->str);
+		ph = ph->next;
+		n++;
+	}
+	return (n);
 }
