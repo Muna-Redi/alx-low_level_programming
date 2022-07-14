c9ae591f0e1189e16c42c7523f882a18d3799f87
Muna-Redi <francischym@gmail.com>
2022-07-15 00:14:40 +0100

    add node template

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* add_node - adds a node tot the begining of a list
+* @head: pointer tot the first node
+* @str: string
+* Return: address of new node successfull and NULL if failed
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+
+}
