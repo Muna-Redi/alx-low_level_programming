8131bc7c218f07f5024bdac4507721410c4187d2
Muna-Redi <francischym@gmail.com>
2022-07-18 19:23:27 +0100

    malloc'ed trav

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,6 +10,9 @@ size_t print_listint(const listint_t *h)
 	listint_t *trav;
 	size_t n = 0;
 
+	trav = malloc(sizeof(listint_t));
+	if (trav == NULL)
+		return (0);
 	trav = NULL;
 	*trav = *h;
 	while (trav!= NULL)
@@ -19,4 +22,5 @@ size_t print_listint(const listint_t *h)
 		n++;
 	}
 	return (n);
+	free(trav);
 }
