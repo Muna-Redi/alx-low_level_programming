4155a93cb928216a6537a1e426095306f4018f44
Muna-Redi <francischym@gmail.com>
2022-07-18 19:28:19 +0100

    no malloc

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,20 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	listint_t *trav;
 	size_t n = 0;
 
-	trav = malloc(sizeof(listint_t));
-	if (trav == NULL)
-		return (0);
-	trav = NULL;
-	*trav = *h;
-	while (trav!= NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", trav->n);
-		trav = trav->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		n++;
 	}
 	return (n);
-	free(trav);
 }
