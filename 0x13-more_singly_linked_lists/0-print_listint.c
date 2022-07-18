f71549d7254cb7c9d0cff033c68ca283d68fe90d
Muna-Redi <francischym@gmail.com>
2022-07-18 19:09:33 +0100

    printing lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a list
+ * @h: head of the list
+ * Return: size of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *trav;
+	size_t n = 0;
+
+	*trav = *h;
+	while (trav!= NULL)
+	{
+		printf("%d\n", trav->n);
+		trav = trav->next;
+		n++;
+	}
+	return (n);
+}
+
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+
+#define LISTS_H
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+/**
+ * struct listint_s - a single linked list
+ * @n: integer value of each node
+ * @next: pointer to the next node in the list
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
