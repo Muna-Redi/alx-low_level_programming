312d282330cc9a601aef2c20bc066ef60f1b7070
Muna-Redi <francischym@gmail.com>
2022-07-19 16:57:00 -0700

    nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,14 +8,22 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *node
 	unsigned int i = 0;
 
-	while (i < index)
+	node = head;
+	if (node == NULL)
+		return (NULL);
+	while (i <= index)
 	{
-		if (head->next == NULL)
-			return (NULL);
-		head = head->next
+		if (i == index)
+			node = head;
+		node = head->next
 		i++;
 	}
-	return (head);
+	if (node == NULL)
+		return (NULL);
+	else
+		return (node);
+
 }
