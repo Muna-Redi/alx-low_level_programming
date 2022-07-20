13ae40e5973f739a27d82aebe8f9ef43bea0b756
Muna-Redi <francischym@gmail.com>
2022-07-20 15:22:20 +0100

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,6 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newnode->next = temp->next;
 	temp = newnode;
-	return (newnode);
+	return (temp);
 	free(newnode);
 }
