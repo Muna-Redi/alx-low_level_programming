57bf22185b1785b5aa8ed421527258cbc0e58dea
Muna-Redi <francischym@gmail.com>
2022-07-19 17:15:22 -0700

    the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	listint_t *node;
 
 	node = head;
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		node = head;
 		if (i != index)
 		{
-			head = head->next
+			head = head->next;
 		}
 		i++;
 	}
