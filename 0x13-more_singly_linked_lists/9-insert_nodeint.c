9142c5f6040dde21769d036304c61df32e3628b3
Muna-Redi <francischym@gmail.com>
2022-07-20 02:31:44 -0700

    returning address of newnode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,30 +9,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *newnode;
+	listint_t *newnode, *temp;
+	unsigned int i = 0;
 
 	temp = *head;
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
-	if (index == 0)
+	if (idx == 0)
 	{
 		newnode->next = temp;
 		*head = newnode;
 		return (*head);
+		free(newnode);
 	}
-	while (idx > 1)
+	while (i < idx)
 	{
 		temp = temp->next;
-		idx--;
-		if (temp)
+		if (temp == NULL)
 		{
 			free(newnode);
 			return (NULL);
 		}
+		i++;
 	}
 	newnode->next = temp->next;
 	temp->next = newnode;
-	return (*head);
+	return (newnode);
+	free(newnode);
 }
