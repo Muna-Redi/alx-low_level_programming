1b2405797cf7fd6ad03c917a7d1928bc6ea3b57c
Muna-Redi <francischym@gmail.com>
2022-07-20 19:11:13 +0100

    delete index node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	current = *head;
-	for (i = 0; i < index; i++)
+	for (i = 0; i <= index; i++)
 	{
 		if (current->next == NULL)
 			return (-1);
