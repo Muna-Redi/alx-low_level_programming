8c728c25a1517dfb36dda002957ddf79ed7f13b7
Muna-Redi <francischym@gmail.com>
2022-07-21 23:39:08 +0100

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int uint = 0, power = 1, len = 0;
+	unsigned int power = 1, uint = 0, len = 0;
 
 	if (b == NULL)
 		return (0);
-	for (; b[len] != '\0'; len++)
-		;
-	while (len >= 0)
+
+	while (b[len])
+		len++;
+	while (len)
 	{
-		if ((b[len - 1] != '0') &&( b[len - 1] != '1'))
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
+
 		if (b[len - 1] == '1')
-			uint = (uint + power) + '0';
+			uint += power;
 		power *= 2;
-		len -= 1;
+		len--;
 	}
 	return (uint);
 }
