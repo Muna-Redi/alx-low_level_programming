b3e02b09b97f39c64d047b2382c6f54d13fa9317
Muna-Redi <francischym@gmail.com>
2022-07-25 19:05:29 -0700

    returning 1 and -1 if filed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,10 +21,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file_open < 0)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	while (*(text_content + i))
 		i++;
 	file_append = write(file_open, text_content, i);
 	close(file_open);
-	return (file_append);
+	if (file_append < 0)
+		return (-1);
+	return (1);
 }
