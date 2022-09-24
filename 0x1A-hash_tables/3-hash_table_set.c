79e300b7099ff1b0a193a294edeea892f3316090
Muna-Redi <francischym@gmail.com>
2022-09-24 03:22:44 +0100

    hash_table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,51 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_set - A function of that adds an element to the hash-table
-* @key: The key of the element to be added
-* @value: Value of the element
-* Return: 1 if success and 0 if otherwise
-*/
+ * hash_table_set - A function that sets a key value pair in the hash table.
+ * @ht: A pointer to hash table to set in.
+ * @key: The key to set in hash table.
+ * @value: The value to set as hash_node's value.
+ * Return: 1 on success, or 0 on failure.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx, ht_size;
-	char *val_copy = NULL;
-	hash_node_t *new_element = NULL;
+	unsigned long int index = 0;
+	char *value_dup = NULL, *key_dup = NULL;
+	hash_node_t *new_node = NULL, *tmp_node = NULL;
 
-	if (key == NULL || strlen(key) == 0)
+	if (!ht || !key || !value)
 		return (0);
-
-	ht_size = ht->size;
-	idx = key_index((unsigned char *)key, ht_size);
-	if (idx > ht->size)
+	else if (strlen(key) == 0)
 		return (0);
-
-	new_element = malloc(sizeof(hash_node_t));
-	if (new_element == NULL)
+	value_dup = strdup(value);
+	key_dup = strdup(key);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
 		return (0);
-
-	val_copy = strdup(value);
-	new_element->key = (char *)key;
-	new_element->value = val_copy;
-	new_element->next = NULL;
-
-	if (ht->array[idx] == NULL)
-		ht->array[idx] = new_element;
-	else
+	new_node->key = key_dup;
+	new_node->value = value_dup;
+	new_node->next = NULL;
+	index = key_index((unsigned char *)key, ht->size);
+	if ((ht->array)[index] != NULL)
 	{
-		new_element->next = ht->array[idx];
-		ht->array[idx] = new_element;
+		tmp_node = (ht->array)[index];
+		while (tmp_node)
+		{
+			if (strcmp(tmp_node->key, key_dup) == 0)
+			{
+				free(ht->array[index]->value);
+				ht->array[index]->value = value_dup;
+				free(key_dup);
+				free(new_node);
+				return (1);
+			}
+			tmp_node = tmp_node->next;
+		}
+		tmp_node = (ht->array)[index];
+		new_node->next = tmp_node;
+		(ht->array)[index] = new_node;
 	}
-
+	else
+		(ht->array)[index] = new_node;
 	return (1);
 }
